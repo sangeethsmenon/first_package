8928d3f15d4635f93fdf70caeae55c57e33aebd7
Sangeeth Menon <sangeethmenon79@gmail.com>
2023-09-18 20:23:53 +0200

    Documentation using roxygen2 for Euclidean algorithm is done

--- first_package/R/dijkstra.R
+++ first_package/R/dijkstra.R
@@ -1,4 +1,26 @@
+#' Dijkstra's Algorithm 
+#'
+#' The algorithm takes a graph and an initial node and calculates the shortest path from the
+#"initial node to every other node in the graph
+#'
+#' @param graph A data frame representing the weighted graph with columns:
+#'   - `v1`: The starting node of an edge.
+#'   - `v2`: The ending node of an edge.
+#'   - `w`: The weight of the edge.
+#' @param init_node The initial node from which to find the shortest paths.
+#'
+#' @return A numeric vector of length equal to the number of nodes in the graph.
+#'   Each element represents the shortest distance from the initial node to the
+#'   corresponding node in the graph.
+#'
+#' @references
+#' Wikipedia: Dijkstra's Algorithm - https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+#'
+#' @export
 dijkstra <- function(graph, init_node) {
+  stopifnot(is.data.frame(graph), all(c("v1", "v2", "w") %in% colnames(graph)))
+  
+  stopifnot(is.numeric(init_node), length(init_node) == 1)
   actual_vertices <- unique(c(graph$v1, graph$v2))
   no_vertices <- length(actual_vertices)
   short <- rep(Inf, no_vertices)
@@ -27,26 +49,3 @@ dijkstra <- function(graph, init_node) {
   
   return(short)
 }
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
--- first_package/R/euclidean.R
+++ first_package/R/euclidean.R
@@ -1,13 +1,24 @@
+#' Euclidean Algorithm for Greatest Common Divisor (GCD)
+#'
+#' This function calculates the greatest common divisor (GCD) of two positive integers
+#' using the Euclidean algorithm.
+#'
+#' @param n A positive numeric value.
+#' @param m A positive numeric value.
+#'
+#' @return The greatest common divisor (GCD) of n and m, which is a positive integer.
+#'
+#' @references
+#' Wikipedia: Euclidean Algorithm - https://en.wikipedia.org/wiki/Euclidean_algorithm
+#'
+#' @export
+
 euclidean <- function(n, m) {
-  stopifnot(is.numeric(n),is.numeric(m))
-  least_num <- min(n, m)
-  output <- 1
-  for (i in 1:least_num) {
-    if (n %% i == 0 && m %% i == 0) {
-      output <- i
-    }
+  stopifnot(is.numeric(n), is.numeric(m))
+  while (m != 0) {
+    output<-n%%m
+    n<-m
+    m<-output
   }
-  return(output)
+  return(n)
 }
-
-
